58e45c91d93e65b54f4f0d25d7b3f9a5b002fa3a
liuguoliang131 <haoguozhi233@sina.cn>
2022-08-29 18:01:02 +0800

    添加server

---
+++ server/index.js
@@ -0,0 +1,22 @@
+const http = require("http");
+
+const app = http.createServer((req, res) => {
+  // 查看收到的请求地址
+  console.log('req.url:', req.url, 'res:', res);
+  // 收到的url信息
+  console.log(req.rawHeaders);
+  if (req.url === "/account/test") {
+    res.end("hello world!!");
+
+  } else {
+    console.log('url:', req.url)
+    let responseJson = JSON.stringify({ success: true })
+    res.end(responseJson)
+  }
+
+});
+
+app.listen(9000, "localhost", () => {
+  console.log("localhost:9000开启服务");
+
+});
