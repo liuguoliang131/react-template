a23c66bc6855e2714ec7f69d0a947aa1b74677a5
liuguoliang131 <haoguozhi233@sina.cn>
2022-08-30 19:20:21 +0800

    mapDispatchToProps

--- src/pages/login/index.jsx
+++ src/pages/login/index.jsx
@@ -1,10 +1,16 @@
 import React from 'react'
 import { useNavigate } from 'react-router-dom'
+import { connect } from 'react-redux/es/exports'
 const Login = (props) => {
   console.log('login')
   const navigate = useNavigate()
   const handLogin = () => {
     console.log('this', props)
+    // eslint-disable-next-line react/prop-types
+    props.setToken({
+      type: 'token',
+      token: 'xxxx'
+    })
     navigate('/layout/home', { state: { token: 'xxxxx' }, replace: false })
   }
   return (
@@ -14,5 +20,14 @@ const Login = (props) => {
     </>
   )
 }
-
-export default Login
+const mapDispatchToProps = (dispatch) => {
+  return {
+    setToken (action) {
+      dispatch(action)
+    },
+    setUserInfo (action) {
+      dispatch(action)
+    }
+  }
+}
+export default connect(null, mapDispatchToProps)(Login)
