6c87e51fb70bb2e7bc0447683464530081048d82
liuguoliang131 <haoguozhi233@sina.cn>
2022-08-30 16:29:39 +0800

    添加postcss-loader和postcss-px-to-viewport

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -148,13 +148,18 @@ module.exports = function (webpackEnv) {
                 // which in turn let's users customize the target behavior as per their needs.
                 'postcss-normalize',
                 require('postcss-px-to-viewport')({
-                  viewportWidth: 1920,
-                  viewportHeight: 1080,
-                  unitPrecision: 3,
-                  viewportUnit: 'vw',
-                  selectorBlackList: [],
-                  minPxelValue: 1,
-                  mediaQuery: false
+                  unitToConvert: "px", // 要转化的单位
+                  viewportWidth: 375, // UI设计稿的宽度
+                  unitPrecision: 6, // 转换后的精度，即小数点位数
+                  propList: ["*"], // 指定转换的css属性的单位，*代表全部css属性的单位都进行转换
+                  viewportUnit: "vw", // 指定需要转换成的视窗单位，默认vw
+                  fontViewportUnit: "vw", // 指定字体需要转换成的视窗单位，默认vw
+                  // selectorBlackList: ["wrap"], // 指定不转换为视窗单位的类名，
+                  minPixelValue: 1, // 默认值1，小于或等于1px则不进行转换
+                  mediaQuery: true, // 是否在媒体查询的css代码中也进行转换，默认false
+                  replace: true, // 是否转换后直接更换属性值
+                  // exclude: [/node_modules/], // 设置忽略文件，用正则做目录名匹配
+                  landscape: false // 是否处理横屏情况
                 })
               ]
               : [
@@ -170,13 +175,18 @@ module.exports = function (webpackEnv) {
                   },
                 ],
                 require('postcss-px-to-viewport')({
-                  viewportWidth: 1920,
-                  viewportHeight: 1080,
-                  unitPrecision: 3,
-                  viewportUnit: 'vw',
-                  selectorBlackList: [],
-                  minPxelValue: 1,
-                  mediaQuery: false
+                  unitToConvert: "px", // 要转化的单位
+                  viewportWidth: 375, // UI设计稿的宽度
+                  unitPrecision: 6, // 转换后的精度，即小数点位数
+                  propList: ["*"], // 指定转换的css属性的单位，*代表全部css属性的单位都进行转换
+                  viewportUnit: "vw", // 指定需要转换成的视窗单位，默认vw
+                  fontViewportUnit: "vw", // 指定字体需要转换成的视窗单位，默认vw
+                  // selectorBlackList: ["wrap"], // 指定不转换为视窗单位的类名，
+                  minPixelValue: 1, // 默认值1，小于或等于1px则不进行转换
+                  mediaQuery: true, // 是否在媒体查询的css代码中也进行转换，默认false
+                  replace: true, // 是否转换后直接更换属性值
+                  // exclude: [/node_modules/], // 设置忽略文件，用正则做目录名匹配
+                  landscape: false // 是否处理横屏情况
                 })
               ],
           },
